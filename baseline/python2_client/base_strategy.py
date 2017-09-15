dcc6095f86278ca3687cda8690376efa16ab7e82
Stycenko Ilia <stitsenco@gmail.com>
2017-09-15 22:38:21 +0300

    bases deleted from baselines

--- baseline/python2_client/base_strategy.py
+++
@@ -1,7 +0,0 @@
-class BaseStrategy(object):
-    def __init__(self, debug, type):
-        self.debug = debug.log
-        self.type = type
-
-    def on_tick(self, my_elevators, my_passengers, enemy_elevators, enemy_passengers):
-       pass
--- baseline/python3_client/base_strategy.py
+++
@@ -1,8 +0,0 @@
-class BaseStrategy(object):
-    debug = None
-
-    def set_debug(self, debug):
-        self.debug = debug.log
-
-    def on_tick(self, passengers, elevators):
-        pass
