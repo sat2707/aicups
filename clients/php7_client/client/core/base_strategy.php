68f9780abcb9c523a3c84821050dedbdb8115eeb
Ilya Agafonov <tairesh.rus@gmail.com>
2017-09-17 16:19:23 +0500

    Fixed BaseStrategy::log method

--- clients/php7_client/client/core/base_strategy.php
+++ clients/php7_client/client/core/base_strategy.php
@@ -1,9 +1,16 @@
 <?php
     class BaseStrategy {
+        
+        protected $debug;
+                
         function __construct($debug) {
-            $this->debug = $debug->log;
+            $this->debug = $debug;
         }
-
+        
+        protected function log($text) {
+            $this->debug->log($text);
+        }
+        
         function on_tick($my_passengers, $my_elevators, $enemy_passengers, $enemy_elevators) {
             return;
         }
