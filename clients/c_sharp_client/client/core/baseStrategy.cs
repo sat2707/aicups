cee83e15f403c1757743ed43823cbb884f3efe63
Stycenko Ilia <stitsenco@gmail.com>
2017-09-18 16:56:01 +0300

    c#, no strategies in clients

---
+++ clients/c_sharp_client/client/core/baseStrategy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Api;
+using System.Collections.Generic;
+namespace client
+{
+	public abstract class baseStrategy
+	{
+		private DebugClass debug;
+	
+		public DebugClass Debug
+		{
+			set { debug = value; }
+		}
+
+		public baseStrategy()
+		{
+		}
+			
+		public abstract void onTick(List<Passenger> myPassengers, List<Elevator> myElevators, List<Passenger> enemyPassengers, List<Elevator> enemyElevators);
+
+		public void log(Object obj)
+		{
+			debug.log (obj);
+		}
+	}
+}
+
