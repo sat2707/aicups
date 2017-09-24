ce266b002d539e9478e19a0e722012a4879d598e
Marat <lastvar@gmail.com>
2017-09-24 04:25:32 +0300

    minor fix of merge mistake

--- clients/kotlin_client/client/src/main/kotlin/core/Client.kt
+++ clients/kotlin_client/client/src/main/kotlin/core/Client.kt
@@ -15,7 +15,7 @@ class Client(private val host: String, private val port: Int, private val soluti
   private val parser = JSONParser()
   private var clientSocket: SocketChannel? = null
 
-  private fun parseString(jsonString: String): JSONObject? {
+  private fun parseString(jsonString: String?): JSONObject? {
     if (jsonString == null) {
       return null
     }
