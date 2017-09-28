c1f0f1b392e3ad4a6738ce03f810da9af3cea637
Daramant <daramant@mail.ru>
2017-09-29 02:10:08 +0300

    Fix formatting.

--- localrunner/world/core/game_objects/passenger.py
+++ localrunner/world/core/game_objects/passenger.py
@@ -161,11 +161,11 @@ class Passenger(object):
             if self.elevator.floor == self.floor and self.elevator.is_filling():
                 if self.elevator.x < 0 and self.x > self.elevator.x:
                     self.move(x=sign(self.elevator.x))
-				elif self.elevator.x < 0 and self.x < self.elevator.x:
+                elif self.elevator.x < 0 and self.x < self.elevator.x:
                     self.move(x=-sign(self.elevator.x))
                 elif self.elevator.x > 0 and self.x < self.elevator.x:
                     self.move(x=sign(self.elevator.x))
-				elif self.elevator.x > 0 and self.x > self.elevator.x:
+                elif self.elevator.x > 0 and self.x > self.elevator.x:
                     self.move(x=-sign(self.elevator.x))
                 elif self.elevator.floor == self.from_floor and self.elevator.can_enter():
                     self.elevator.enter(self)
