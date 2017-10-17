f1643fb1a05879c4ec4cc5f1a694b464961dc312
AlexB <ua3mqj@gmail.com>
2017-10-17 15:22:59 +0300

    last changes

--- clients/elixir_client/lib/elixir_client/client.ex
+++ clients/elixir_client/lib/elixir_client/client.ex
@@ -18,11 +18,11 @@ defmodule ElixirClient.Client do
     {:ok, socket} = :gen_tcp.connect(String.to_char_list(host),
                                      port, [:binary, {:packet, :line}, {:active, true}])
 
-    json = Poison.encode!(%{solution_id: "#{solution_id}"})
+    json = Poison.encode!(%{solution_id: solution_id}) <> "\n"
     msg = String.to_char_list(json)
     :gen_tcp.send(socket, msg)
 
-    {:ok, %{options: options, socket: socket, transmitting: false}}
+    {:ok, %{options: options, socket: socket, transmitting: false, sdata: ""}}
   end
 
   def handle_cast({:send_actions, json_actions}, state) do
@@ -34,22 +34,52 @@ defmodule ElixirClient.Client do
   end
 
   def handle_info({:tcp, _port, msg}, state) do
-    json = Poison.decode!(msg)
-    handle_json(state, json)
+    rcvd = state.sdata <> msg
+
+    case String.contains?(rcvd, "\n") do
+      true ->
+        array_of_json = String.split(rcvd, "\n")
+        # Logger.debug ">>>> array_of_json=#{inspect array_of_json}"
+        # spawn(__MODULE__, :parse_jsons, [array_of_json])
+        parse_jsons(array_of_json)
+
+        {:noreply, %{state | sdata: List.last(array_of_json) }}
+      _else ->
+        {:noreply, %{state | sdata: rcvd }}
+    end   
   end
 
-  def handle_info(msg, state) do
+  def handle_info({:data_to_decode, data}, state) do
+    # Logger.info "handle_info data_to_decode data=#{inspect data}"
+    json = Poison.decode!(data)
+    handle_json(state, json)
+    # {:noreply, state}
+  end
+
+  def handle_info(_msg, state) do
     # Logger.info "handle_info msg=#{inspect msg}"
 
     {:noreply, state}
   end
 
+  def parse_jsons([""]) do
+    ""
+  end
+  def parse_jsons([not_ended_tail]) do
+    not_ended_tail
+  end
+  def parse_jsons([head | tail]) do
+    send(self(), {:data_to_decode, head})
+    parse_jsons(tail)
+  end
+
   defp handle_json(state, %{"message" => "beginning"} = _json) do
-    # Logger.info "client beginning"
+    Logger.info "client beginning"
     {:noreply, %{state | transmitting: true}}
   end
 
   defp handle_json(state, %{"message" => "down"} = _json) do
+    Logger.info "client down"
     :gen_tcp.close(state.socket)
     {:noreply, %{state | socket: nil, transmitting: false}}
   end
@@ -58,10 +88,6 @@ defmodule ElixirClient.Client do
   defp handle_json(state, json) do
     # Logger.info "client rcv json=#{inspect json}"
     ElixirClient.Api.generate_actions(json)
-    {:noreply, state}
-  end
-
-  defp handle_json(state, _json) do
     {:noreply, state}
   end
 
--- clients/elixir_client/lib/strategy.ex
+++ clients/elixir_client/lib/strategy.ex
@@ -21,11 +21,11 @@ defmodule Strategy do
   @exiting              6 # выходит из лифта
 
   def on_tick(my_elevators, my_passengers, enemy_elevators, enemy_passengers) do
-    Enum.map(my_elevators, fn(el) ->
-                             Elevator.go_to_floor(el, 1)
-                           end)
-    hd(my_passengers)
-    |> Passenger.set_elevator(hd(my_elevators))
+    # Enum.map(my_elevators, fn(el) ->
+    #                          Elevator.go_to_floor(el, 1)
+    #                        end)
+    # hd(my_passengers)
+    # |> Passenger.set_elevator(hd(my_elevators))
   end
 
   def start_link() do
